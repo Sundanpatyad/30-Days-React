3a0747cb093bd4a93fb5b2cc76e96f67945513ea
Sundanpatyad <sundansharma600@gmail.com>
2024-06-12 14:33:57 +0530

    Use State

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Mobile from './Mobile'
 import Data from './Data'
+import Form from './Form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Form/>
    <Mobile></Mobile>
    <Data/>
   </React.StrictMode>,
